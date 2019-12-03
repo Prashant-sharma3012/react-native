ede2f3f0f26ae27e481c6413c2eeb0348cedb665
Prashant Sharma <prashant.sharma@enquero.com>
2019-12-03 16:22:32 +0530

    added data to cart and items getting removed

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { connect } from "react-redux";
 import { removeFromCart } from "../actions/cart.action";
 
 class CartScreen extends Component {
+
+  static navigationOptions = (navigationData) => {
+    return {
+      headerTitle: "Cart"
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        {
-          this.props.items.map((item, index) => (
+        {this.props.items.length === 0
+          ? <Text style={styles.emptyMessage}> No Items Added to cart</Text>
+          : this.props.items.map((item, index) => (
             <View style={styles.item} key={index}>
               <Text>{item.productName}</Text>
               <Text>{item.price}</Text>
               <Text>{item.discount}</Text>
+              <Button title="Remove" onPress={() => this.props.removeFromCart(item)}/>
             </View>
           ))
         }
@@ -22,13 +31,19 @@ class CartScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     display: "flex",
     justifyContent: "center",
+    flexDirection: "column"
   },
-  item:{
+  item: {
     alignItems: "center",
     margin: 30
+  },
+  emptyMessage: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf:"center"
   }
 })
 
